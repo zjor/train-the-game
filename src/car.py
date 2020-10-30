ad2f0cdb49008b25354ff789b164a7453aa405c6
Sergey Royz <zjor.se@gmail.com>
2020-10-30 23:05:59 +0100

    ray.py removed

--- src/car.py
+++ src/car.py
@@ -4,7 +4,24 @@ import numpy as np
 from math import sin, cos, pi
 
 from constants import Colors
-from ray import Ray
+
+class Ray:
+    def __init__(self, origin=(.0, .0), theta=.0):
+        self.origin = origin
+        self.theta = theta
+
+
+    def get_collision(self, mask, threshold):
+        d = 0
+        x, y = self.origin
+        t_x, t_y = x, y
+        bounds = mask.get_rect()
+
+        while d < threshold and bounds.collidepoint(t_x, t_y) and mask.get_at((int(t_x), int(t_y))) == 0:
+            t_x, t_y = x + d * cos(self.theta), y - d * sin(self.theta)            
+            d += 1
+            
+        return ((int(t_x), int(t_y)), d)
 
 
 class Car:
